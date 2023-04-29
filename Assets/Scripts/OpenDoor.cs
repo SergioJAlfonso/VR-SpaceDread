e650060b319bd7f20aa7dce8ad8151ad6db35780
Ángel López <62568478+angelo066@users.noreply.github.com>
2023-04-29 17:10:49 +0200

    Puerta cierra y cristal (Mira laura a ver si te gusta))

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -6,17 +6,29 @@ public class OpenDoor : MonoBehaviour
 {
     bool opening = false;
 
+    bool closing = false;
+
     float cuantity = 3.0f;
 
     float acumJeje = 0;
+
+    float acumJejeBajar = 0;
 
     public void open()
     {
         opening = true;
     }
 
+    public void close()
+    {
+        closing = true;
+    }
+
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.O)) open();
+        if (Input.GetKeyDown(KeyCode.C)) close();
+
 
         if (opening)
         {
@@ -25,9 +37,22 @@ public class OpenDoor : MonoBehaviour
 
             if (acumJeje >= cuantity) opening = false;
         }
+
+        if (closing)
+        {
+            transform.Translate(new Vector3(0, -0.01f, 0));
+            acumJejeBajar += 0.01f;
+
+            if (acumJejeBajar >= cuantity) closing = false;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         open(); 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        close();
     }
 }
