dc573bb1b04190f1932dd6e20260054989e48307
Sergio Molinero <sergimol@ucm.es>
2023-04-30 12:35:55 +0200

    pushea para que el mundo lo vea
    que la vida con un filtro no es tan fea

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -42,16 +42,16 @@ public class OpenDoor : MonoBehaviour
 
         if (opening)
         {
-            transform.Translate(new Vector3(0, 0.01f, 0));
-            acumJeje += 0.01f;
+            transform.Translate(new Vector3(0, 0.05f, 0));
+            acumJeje += 0.05f;
 
             if (acumJeje >= cuantity) opening = false;
         }
 
         if (closing)
         {
-            transform.Translate(new Vector3(0, -0.01f, 0));
-            acumJejeBajar += 0.01f;
+            transform.Translate(new Vector3(0, -0.05f, 0));
+            acumJejeBajar += 0.05f;
 
             if (acumJejeBajar >= cuantity) closing = false;
         }
